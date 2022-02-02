fa7a88088ad05ef5f8e959b161cbeae6dad44e1d
soblab <soblab@Whit209-06.it.farmingdale.edu>
2022-02-02 12:53:28 -0500

    Add hello world commit

--- src/main/java/com/mycompany/csc229_lab01_work/LabWork.java
+++ src/main/java/com/mycompany/csc229_lab01_work/LabWork.java
@@ -3,4 +3,11 @@ package com.mycompany.csc229_lab01_work;
 
 public class LabWork {
     
+
+    public static void main(String[] args)
+    {
+        System.out.println("hello world");
+    }
+
+
 }
