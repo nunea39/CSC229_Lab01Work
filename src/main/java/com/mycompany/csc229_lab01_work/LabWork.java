1b5462397a1c087e5ce93660e33d4385e7d489e7
soblab <soblab@Whit209-06.it.farmingdale.edu>
2022-02-02 13:31:40 -0500

    Add labs to commit

--- src/main/java/com/mycompany/csc229_lab01_work/LabWork.java
+++ src/main/java/com/mycompany/csc229_lab01_work/LabWork.java
@@ -1,12 +1,49 @@
 package com.mycompany.csc229_lab01_work;
 
 
+
 public class LabWork {
     
+    //Task01
+    static int getSumofPrimes(int n){
+        int sum=0;
+        for(int i=2; i<=n; i++)
+            if(isPrim(i))
+                sum = sum+ i;       
+        return sum;
+    }
+    static boolean isPrim(int x){
+        for(int i=2; i<x; i++)
+            if(x%i == 0)
+                return false;
+        return true;
+    }
+    
+    //Task02
+    static int getSumofPrimesWithDigit(int n){
+        int sum=0;
+        for(int i=2; i<=n; i++)
+            if(isPrim(i) && checkDigit(i,3))
+                sum = sum+ i;       
+        return sum;
+    }
+    static boolean checkDigit(int x, int digit){
+        String str = ""+x;
+        return str.contains(""+digit);        
+    }
+    
+    //Task03
+    static int getSumofPrimesWith2Digit(int n){
+        int sum=0;
+        for(int i=2; i<=n; i++)
+            if(isPrim(i) && checkDigit(i,3) && checkDigit(i,5))
+                sum = sum+ i;       
+        return sum;
+    }
 
     public static void main(String[] args)
     {
-        System.out.println("hello world");
+        System.out.println(getSumofPrimesWith2Digit (100) );
     }
 
 
